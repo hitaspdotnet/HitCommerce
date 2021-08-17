7a48a3228fb580ad7bd17fde6ae70fb52e9aac0a
Mostafa Forouji <hitasp@outlook.com>
2021-08-18 03:12:04 +0430

    core -> countriesAppService

--- modules/Hitasp.HitCommerce.Core/src/Hitasp.HitCommerce.Core.Application/Countries/CountryAppService.cs
+++ modules/Hitasp.HitCommerce.Core/src/Hitasp.HitCommerce.Core.Application/Countries/CountryAppService.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Linq.Dynamic.Core;
 using Microsoft.AspNetCore.Authorization;
-using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
-using Volo.Abp.Domain.Repositories;
 using Hitasp.HitCommerce.Core.Permissions;
-using Hitasp.HitCommerce.Core.Countries;
 
 namespace Hitasp.HitCommerce.Core.Countries
 {
@@ -17,17 +12,19 @@ namespace Hitasp.HitCommerce.Core.Countries
     [Authorize(CorePermissions.Countries.Default)]
     public class CountriesAppService : ApplicationService, ICountriesAppService
     {
-        private readonly ICountryRepository _countryRepository;
+        protected ICountryRepository CountryRepository { get; }
+        protected ICountryManager CountryManager { get; }
 
-        public CountriesAppService(ICountryRepository countryRepository)
+        public CountriesAppService(ICountryRepository countryRepository, ICountryManager countryManager)
         {
-            _countryRepository = countryRepository;
+            CountryRepository = countryRepository;
+            CountryManager = countryManager;
         }
 
         public virtual async Task<PagedResultDto<CountryDto>> GetListAsync(GetCountriesInput input)
         {
-            var totalCount = await _countryRepository.GetCountAsync(input.FilterText, input.Name, input.Code3, input.IsBillingEnabled, input.IsShippingEnabled);
-            var items = await _countryRepository.GetListAsync(input.FilterText, input.Name, input.Code3, input.IsBillingEnabled, input.IsShippingEnabled, input.Sorting, input.MaxResultCount, input.SkipCount);
+            var totalCount = await CountryRepository.GetCountAsync(input.FilterText, input.Name, input.Code3, input.IsBillingEnabled, input.IsShippingEnabled);
+            var items = await CountryRepository.GetListAsync(input.FilterText, input.Name, input.Code3, input.IsBillingEnabled, input.IsShippingEnabled, input.Sorting, input.MaxResultCount, input.SkipCount);
 
             return new PagedResultDto<CountryDto>
             {
@@ -38,33 +35,34 @@ namespace Hitasp.HitCommerce.Core.Countries
 
         public virtual async Task<CountryDto> GetAsync(Guid id)
         {
-            return ObjectMapper.Map<Country, CountryDto>(await _countryRepository.GetAsync(id));
+            return ObjectMapper.Map<Country, CountryDto>(await CountryRepository.GetAsync(id));
         }
 
         [Authorize(CorePermissions.Countries.Delete)]
         public virtual async Task DeleteAsync(Guid id)
         {
-            await _countryRepository.DeleteAsync(id);
+            await CountryRepository.DeleteAsync(id);
         }
 
         [Authorize(CorePermissions.Countries.Create)]
         public virtual async Task<CountryDto> CreateAsync(CountryCreateDto input)
         {
+            var country = await CountryManager.CreateAsync(input.Name, input.Code3, input.IsBillingEnabled, input.IsShippingEnabled);
+            country = await CountryRepository.InsertAsync(country, autoSave: true);
 
-            var country = ObjectMapper.Map<CountryCreateDto, Country>(input);
-
-            country = await _countryRepository.InsertAsync(country, autoSave: true);
             return ObjectMapper.Map<Country, CountryDto>(country);
         }
 
         [Authorize(CorePermissions.Countries.Edit)]
         public virtual async Task<CountryDto> UpdateAsync(Guid id, CountryUpdateDto input)
         {
+            var country = await CountryRepository.GetAsync(id);
+            await CountryManager.RenameAsync(country, input.Name);
+            await CountryManager.ChangeCodeAsync(country, input.Code3);
+            country.IsBillingEnabled = input.IsBillingEnabled;
+            country.IsShippingEnabled = input.IsShippingEnabled;
 
-            var country = await _countryRepository.GetAsync(id);
-            ObjectMapper.Map(input, country);
-            country = await _countryRepository.UpdateAsync(country, autoSave: true);
-            return ObjectMapper.Map<Country, CountryDto>(country);
+            return ObjectMapper.Map<Country, CountryDto>(await CountryRepository.UpdateAsync(country, autoSave: true));
         }
     }
 }
