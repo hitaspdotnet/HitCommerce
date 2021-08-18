224e54578fd8075aed76c29c724b182185783871
Mostafa Forouji <hitasp@outlook.com>
2021-08-19 02:28:29 +0430

    made select2 dynamic #address

--- modules/Hitasp.HitCommerce.Core/src/Hitasp.HitCommerce.Core.Web/Pages/Core/Addresses/editModal.js
+++ modules/Hitasp.HitCommerce.Core/src/Hitasp.HitCommerce.Core.Web/Pages/Core/Addresses/editModal.js
@@ -3,6 +3,11 @@ var abp = abp || {};
 abp.modals.addressEdit = function () {
     var initModal = function (publicApi, args) {
 
+        var address_CountryId = $('#Address_CountryId');
+        var address_StateOrProvinceId = $('#Address_StateOrProvinceId');
+        var address_CityId = $('#Address_CityId');
+        var address_DistrictId = $('#Address_DistrictId');
+        
         var lastNpIdId = '';
         var lastNpDisplayNameId = '';
 
@@ -23,7 +28,23 @@ abp.modals.addressEdit = function () {
         });
 
         publicApi.onOpen(function () {
-            $('#Address_CountryId').select2({
+            
+            address_CountryId.on('change', function (e) {
+                address_StateOrProvinceId.val('').trigger('change');
+                address_CityId.val('').trigger('change');
+                address_DistrictId.val('').trigger('change');
+            });
+
+            address_StateOrProvinceId.on('change', function (e) {
+                address_CityId.val('').trigger('change');
+                address_DistrictId.val('').trigger('change');
+            });
+
+            address_CityId.on('change', function (e) {
+                address_DistrictId.val('').trigger('change');
+            });
+
+            address_CountryId.select2({
                 ajax: {
                     url: abp.appPath + 'api/core/addresses/country-lookup',
                     type: 'GET',
@@ -39,61 +60,64 @@ abp.modals.addressEdit = function () {
                     }
                 }
             });
-        });
-        publicApi.onOpen(function () {
-            $('#Address_StateOrProvinceId').select2({
-                ajax: {
-                    url: abp.appPath + 'api/core/addresses/state-or-province-lookup/' + $('#Address_CountryId').val(),
-                    type: 'GET',
-                    data: function (params) {
-                        return {filter: params.term, maxResultCount: 10}
-                    },
-                    processResults: function (data) {
-                        var mappedItems = _.map(data.items, function (item) {
-                            return {id: item.id, text: item.displayName};
-                        });
 
-                        return {results: mappedItems};
+            address_CountryId.on('select2:select', function (e) {
+                address_StateOrProvinceId.select2({
+                    ajax: {
+                        url: abp.appPath + 'api/core/addresses/state-or-province-lookup/' + address_CountryId.val(),
+                        type: 'GET',
+                        data: function (params) {
+                            return {filter: params.term, maxResultCount: 10}
+                        },
+                        processResults: function (data) {
+                            var mappedItems = _.map(data.items, function (item) {
+                                return {id: item.id, text: item.displayName};
+                            });
+
+                            return {results: mappedItems};
+                        }
                     }
-                }
+                });
             });
-        });
-        publicApi.onOpen(function () {
-            $('#Address_CityId').select2({
-                ajax: {
-                    url: abp.appPath + 'api/core/addresses/city-lookup/' + $('#Address_StateOrProvinceId').val(),
-                    type: 'GET',
-                    data: function (params) {
-                        return {filter: params.term, maxResultCount: 10}
-                    },
-                    processResults: function (data) {
-                        var mappedItems = _.map(data.items, function (item) {
-                            return {id: item.id, text: item.displayName};
-                        });
 
-                        mappedItems.unshift({id: '', text: ' - '});
-                        return {results: mappedItems};
+            address_StateOrProvinceId.on('select2:select', function (e) {
+                address_CityId.select2({
+                    ajax: {
+                        url: abp.appPath + 'api/core/addresses/city-lookup/' + address_StateOrProvinceId.val(),
+                        type: 'GET',
+                        data: function (params) {
+                            return {filter: params.term, maxResultCount: 10}
+                        },
+                        processResults: function (data) {
+                            var mappedItems = _.map(data.items, function (item) {
+                                return {id: item.id, text: item.displayName};
+                            });
+
+                            mappedItems.unshift({id: '', text: ' - '});
+                            return {results: mappedItems};
+                        }
                     }
-                }
+                });
             });
-        });
-        publicApi.onOpen(function () {
-            $('#Address_DistrictId').select2({
-                ajax: {
-                    url: abp.appPath + 'api/core/addresses/district-lookup/' + $('#Address_CityId').val(),
-                    type: 'GET',
-                    data: function (params) {
-                        return {filter: params.term, maxResultCount: 10}
-                    },
-                    processResults: function (data) {
-                        var mappedItems = _.map(data.items, function (item) {
-                            return {id: item.id, text: item.displayName};
-                        });
 
-                        mappedItems.unshift({id: '', text: ' - '});
-                        return {results: mappedItems};
+            address_CityId.on('select2:select', function (e) {
+                address_DistrictId.select2({
+                    ajax: {
+                        url: abp.appPath + 'api/core/addresses/district-lookup/' + address_CityId.val(),
+                        type: 'GET',
+                        data: function (params) {
+                            return {filter: params.term, maxResultCount: 10}
+                        },
+                        processResults: function (data) {
+                            var mappedItems = _.map(data.items, function (item) {
+                                return {id: item.id, text: item.displayName};
+                            });
+
+                            mappedItems.unshift({id: '', text: ' - '});
+                            return {results: mappedItems};
+                        }
                     }
-                }
+                });
             });
         });
 
